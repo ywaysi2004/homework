f7c031b2edb09ba25383e216bc1e9a7206b8db3e
ywaysi2004 <igritsenko36586@gmail.com>
2023-04-28 09:46:56 +0300

    homework_2

---
+++ hw_2.py
@@ -0,0 +1,25 @@
+list1 = []
+list2 = ['winter', 'spring', 'summer']
+list2.append('autumn')
+print(list2)
+list2.remove('spring')
+print(list2)
+list3 = [1, 2, 3, 4, 5]
+all_list = []
+all_list.extend(list2)
+all_list.extend(list3)
+print(all_list)
+list2.insert(0 ,'summer')
+print(list2)
+del all_list[7]
+print(all_list)
+pare_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(pare_list[2:21:2])
+print(pare_list)
+bebe_tuple = ('wow', 911, 'megan')
+print(bebe_tuple)
+name = bebe_tuple[2]
+print(name)
+
+
+
